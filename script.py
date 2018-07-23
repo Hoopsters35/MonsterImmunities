0ccdaf3787b398c8c02539b8aacfcf392f2a3a2a
Justin Knueppel <justinknueppel@gmail.com>
2018-07-23 18:10:43 -0400

    Change to html5lib due to insane issues installing lxml

--- script.py
+++ script.py
@@ -5,11 +5,11 @@ while True:
     query_link = "+".join(query.split())
     url = base_url + query_link
     rq = requests.get(url)
-    search_soup = bs4.BeautifulSoup(rq.text, 'lxml')
+    search_soup = bs4.BeautifulSoup(rq.text, 'html5lib')
     boss_page_url = search_soup.select('ul.Results li.result a.result-link')[0].get('href')
 
     boss_page = requests.get(boss_page_url)
-    boss_soup = bs4.BeautifulSoup(boss_page.text, 'lxml')
+    boss_soup = bs4.BeautifulSoup(boss_page.text, 'html5lib')
 
     poison = boss_soup.select('span[title$="poison."] > img')[0].get('alt')
     deflect = boss_soup.select('span[title$="deflect."] > img')[0].get('alt')
