018512161cbfca82615aa252140c0a72ee81cb91
Hoopsters35 <32350916+Hoopsters35@users.noreply.github.com>
2018-07-23 00:45:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Script to tell you the immunities of a give Runescape boss.
+
+Requires Python 3.7, requests and bs4 modules
---
+++ script.py
@@ -0,0 +1,19 @@
+import requests, bs4
+base_url = 'http://runescape.wikia.com/wiki/Special:Search?query='
+while True:
+    query = input('Enter boss name: ')
+    query_link = "+".join(query.split())
+    url = base_url + query_link
+    rq = requests.get(url)
+    search_soup = bs4.BeautifulSoup(rq.text, 'lxml')
+    boss_page_url = search_soup.select('ul.Results li.result a.result-link')[0].get('href')
+
+    boss_page = requests.get(boss_page_url)
+    boss_soup = bs4.BeautifulSoup(boss_page.text, 'lxml')
+
+    poison = boss_soup.select('span[title$="poison."] > img')[0].get('alt')
+    deflect = boss_soup.select('span[title$="deflect."] > img')[0].get('alt')
+    stun = boss_soup.select('span[title$="stun."] > img')[0].get('alt')
+    drain = boss_soup.select('span[title$="drain."] > img')[0].get('alt')
+
+    print(f'Poison immune: {poison}\nDeflect immunte: {deflect}\nStun immune: {stun}\nDrain immune: {drain}')
